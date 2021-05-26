da37d4a4ef8069e664af33ac6fa786612630252d
Szymon Dziedzic <sz.dziedz@gmail.com>
2021-05-26 20:59:10 +0200

    database test

---
+++ src/tests/Database.test.js
@@ -0,0 +1,17 @@
+import DatabaseSingleton from '../classes/Database';
+import CountryFactory, { Country } from '../classes/Country';
+
+const URL = 'https://covid.ourworldindata.org/data/owid-covid-data.json';
+
+const databaseSingleton = new DatabaseSingleton();
+
+const database = databaseSingleton.getInstance(URL);
+
+test('Database fetch data', async () => {
+	const countryArray = await database.fetchData();
+	expect(database.data).not.toBe(null);
+	expect(countryArray.length).not.toBe(0);
+	countryArray.forEach((country) => {
+		expect(country).toBeInstanceOf(Country);
+	});
+});
